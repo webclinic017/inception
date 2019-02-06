4ebcb682b5ef2ec4c35cea257a4368c88a21209c
Victor Veiga <vveiga@BigMac.local>
2019-02-05 18:21:13 -0800

    First draft of basic intrinsic valuation.

--- get_store_data.py
+++ get_store_data.py
@@ -1,10 +1,12 @@
 import sys
 from basic_utils import *
 from option_recommendations import *
+from unpack_summary import *
 
 if __name__ == '__main__':
     hook = sys.argv[1]
     today_date = str(date.today())
+
     if hook == 'quotes':
         get_quotes(UNIVERSE)
         quotes = flatten_quotes([str(today_date)])
@@ -13,6 +15,7 @@ if __name__ == '__main__':
         for t in UNIVERSE:
             print('Retrieving {0} for {1}'.format(hook, t))
             get_grouped_ds(t, 'summary')
+        unpack_summaries([today_date])
     elif hook == 'options':
         sample_set = sys.argv[2]
         for t in config[sample_set]:
@@ -23,9 +26,12 @@ if __name__ == '__main__':
         #run_options_recommendation()
     elif hook == 'pricing':
         for t in UNIVERSE:
-            print('Retrieving {0} for {1}'.format(hook, t))
-            get_pricing(t, '1m', '5d')
-            get_pricing(t, '1d', '10y')
+            try:
+                print('Retrieving {0} for {1}'.format(hook, t))
+                get_pricing(t, '1m', '5d')
+                get_pricing(t, '1d', '10y')
+            except Exception as e:
+                print(e)
     else:
         print('Please enter a valid option')
         print('Valid options are: quotes, summary, and options')
