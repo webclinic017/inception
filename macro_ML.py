3e2a5fe9187a8f1b2624290027863d9e84b6e775
Victor Veiga <veigaco@gmail.com>
2019-03-28 12:00:47 -0700

    Bug fixing on pre-processing, consistent with px_mom model.

--- macro_ML.py
+++ macro_ML.py
@@ -3,6 +3,7 @@ import sys, os
 from utils.basic_utils import *
 from utils.pricing import *
 from utils.fundamental import *
+from utils import ml_utils as mu
 
 from sklearn import preprocessing
 from sklearn.preprocessing import StandardScaler
@@ -12,14 +13,11 @@ from sklearn.ensemble import forest
 
 from sklearn.model_selection import cross_val_score, cross_validate, train_test_split
 from sklearn.model_selection import GridSearchCV
-from sklearn.feature_selection import SelectFromModel
-from sklearn.utils.validation import column_or_1d
 from sklearn.utils.multiclass import unique_labels
 from sklearn.metrics import confusion_matrix, classification_report
 from sklearn.metrics import accuracy_score, log_loss, precision_recall_fscore_support
 from sklearn.metrics import precision_score, roc_auc_score
 
-from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import VotingClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import ExtraTreesClassifier
@@ -32,10 +30,10 @@ cuts = { '1d': [-1, -0.1, -.02, .02, .1, 1.] }
 cut_range = cuts[freq]
 fwd_ret_labels = ["bear", "short", "neutral", "long", "bull"]
 
+# pricing, refresh once
 benchSL, sectorSL, riskSL, rateSL, bondSL, commSL, currSL = \
     config['benchmarks'], config['sectors'], config['risk'], config['rates'], \
     config['bonds'], config['commodities'], config['currencies']
-
 symbols_list = benchSL + sectorSL + riskSL + rateSL + bondSL + commSL + currSL
 
 keep_bench = excl(benchSL, ['^STOXX50E', '^AXJO'])
@@ -53,9 +51,10 @@ pred_fwd_windows = [20, 60, 120]
 rate_windows = [20, 60]
 sec_windows, stds = [5, 20, 60], 1
 
+# MODEL SPECIFIC FUNCTIONS
 # utility functions
 def create_ds(px_close, context):
-
+    print('create_ds')
     train_model = context['train_model']
     portion = context['portion']
     verbose = context['verbose']
@@ -90,46 +89,45 @@ def create_ds(px_close, context):
 
     return ds_idx, df_large
 
-def pre_process_ds(df, context):
-
-    verbose = context['verbose']
+def pre_process_ds(raw_df, context):
+    print('pre_process_ds')
     train_model = context['train_model']
-    fill_on, imputer_on, scaler_on = \
-        context['fill'], context['impute'], context['scale']
-    test_sz = context['test_size']
     pred_batch = context['predict_batch']
+    fill_on, imputer_on, scaler_on = context['fill'], context['impute'], context['scale']
     (path, train_cols) = context['trained_cols']
+    test_sz, verbose = context['test_size'], context['verbose']
 
-    imputer = SimpleImputer(
-        missing_values=np.nan, strategy='median', copy=False)
     scaler = StandardScaler()
-    X_cols = excl(df.columns, [y_col])
+    imputer = SimpleImputer(
+        missing_values=np.nan,
+        strategy='median', copy=False)
+    X_cols = excl(raw_df.columns, [y_col])
 
-    df.replace([np.inf, -np.inf], np.nan, inplace=True)
-    if fill_on: df.fillna(method=fill_on, inplace=True)
-    if scaler_on: df.loc[:, X_cols] = scaler.fit_transform(df[X_cols])
+    raw_df.replace([np.inf, -np.inf], np.nan, inplace=True)
+    if scaler_on: raw_df[X_cols] = scaler.fit_transform(raw_df[X_cols])
 
-    pred_X = df.iloc[-pred_batch:,:-1].copy() # how far back to predict
-    X_train = X_test = y_train = y_test = None
-
+    pred_X = X_train = X_test = y_train = y_test = None
     if train_model:
-        if fill_on: df[X_cols].fillna(method=fill_on, inplace=True)
+        if fill_on: raw_df.loc[:, X_cols].fillna(method=fill_on, inplace=True)
+
         # discretize forward returns into classes
-        df.dropna(subset=[y_col], inplace=True)
-        df[y_col] = discret_rets(df[y_col], cut_range, fwd_ret_labels)
-        df[y_col] = df[y_col].astype(str)
-        # this seems unnecesary when fill is on
-        if imputer_on: df.loc[:, X_cols] = imputer.fit_transform(df[X_cols])
-        else: df[X_cols].dropna(inplace=True)
-        X, y = df.drop(columns=y_col), df[y_col]
+        raw_df.dropna(subset=[y_col], inplace=True)
+        raw_df.loc[:, y_col] = discret_rets(raw_df[y_col], cut_range, fwd_ret_labels)
+        raw_df.dropna(subset=[y_col], inplace=True) # no nas in y_col
+        print(sample_wgts(raw_df[y_col]))
+        raw_df.loc[:, y_col] = raw_df[y_col].astype(str) # class as string
+
+        if imputer_on: raw_df.loc[:, X_cols] = imputer.fit_transform(raw_df[X_cols])
+        else: raw_df.dropna(inplace=True)
+
+        X, y = raw_df.drop(columns=y_col), raw_df[y_col]
         X_train, X_test, y_train, y_test = train_test_split(
             X, y, test_size=test_sz, random_state=42)
         np.save(path + train_cols, X_train.columns) # save feature order
-        if verbose:
-            y_col_dist = sample_wgts(df[y_col], fwd_ret_labels)
-            print('pre_process_ds >> df_raw Y-var class distribution')
-            print((y_col_dist[fwd_ret_labels]).round(3))
+    else:
+        pred_X = raw_df.iloc[-pred_batch:,:].dropna(axis=0)
 
+    [print(x.shape) for x in (pred_X, X_train, X_test, y_train, y_test) if x is not None]
     return pred_X, X_train, X_test, y_train, y_test
 
 def train_ds(context):
@@ -142,27 +140,30 @@ def train_ds(context):
     grid_search = context['grid_search']
     verbose = context['verbose']
 
-    px_close = get_mults_pricing(include, freq, verbose=verbose);
-    px_close.drop_duplicates(inplace=True)
-    if verbose: print('train_ds >> px_close.shape', px_close.shape)
-
     # create and pre-process datasets
-    px_idx, df_raw = create_ds(px_close, context)
-    pred_X, X_train, X_test, y_train, y_test = pre_process_ds(df_raw, context)
-    if verbose:
-        for x in zip(('df_raw', 'pred', 'X_train', 'y_train', 'X_test', 'y_test'),
-        (df_raw, pred_X, X_train, y_train, X_test, y_test)):
-            print(x[0] + '.shape', x[1].shape)
+    _, df_large = create_ds(px_close, context)
+    pred_X, X_train, X_test, y_train, y_test = pre_process_ds(df_large, context)
 
     # RandomForestClassifier
-    best_params = {
-        'max_features': 'sqrt', 'n_estimators': 100,
-        'random_state': 4}
+    features = X_train.shape[1]
+    best_params = { # best from GridSearch
+        'n_estimators': 50,
+        'max_features': features // 4,
+        'max_depth': 30,
+        'min_samples_split': 2,
+        'min_samples_leaf': 1,
+        'random_state': 2,
+        'n_jobs': -1}
     if grid_search:
         print('GridSearchCV for RandomForestClassifier')
         param_grid = {
-            'n_estimators': [100], 'max_features': ['sqrt'],
-            'random_state': np.arange(0, 5, 1),}
+            'n_estimators': [50],
+            'max_features': [features // x for x in range(1,10,1)],
+            'max_depth': 30,
+            'min_samples_split': 2,
+            'min_samples_leaf': 1,
+            'random_state': np.arange(0, 5, 1),
+        }
         clf = GridSearchCV(RandomForestClassifier(random_state=42),
                            param_grid, n_jobs=-1,
                            cv=5, iid=True, verbose=verbose)
@@ -178,24 +179,29 @@ def train_ds(context):
 
     # MLPClassifier
     best_params = {
-        'activation': 'relu', 'alpha': 0.01, 'hidden_layer_sizes': 95,
-        'learning_rate': 'adaptive', 'max_iter': 200,
-        'random_state': 4, 'solver': 'lbfgs'}
+        'solver': 'sgd',
+        'max_iter': 2000,
+        'activation': 'relu',
+        'alpha': 0.01,
+        'hidden_layer_sizes': (features // 4),
+        'learning_rate': 'adaptive',
+        'random_state': 0, }
     if grid_search:
         print('GridSearchCV for MLPClassifier')
         param_grid = {
-            'solver': ['lbfgs'], # ['lbfgs', 'sgd', 'adam']
-            'max_iter': [200], # [200, 400, 600]
+            'solver': ['sgd'], # ['lbfgs', 'sgd', 'adam']
+            'max_iter': [2000], # [400, 1000, 2000]
             'activation': ['relu'], # ['logistic', 'tanh', 'relu']
-            'alpha': 10.0 ** -np.arange(2, 5, 1), # 10.0 ** -np.arange(2, 5, 1)
+            'alpha': [10.0 ** -2], # 10.0 ** -np.arange(2, 5, 1)
             'learning_rate' : ['adaptive'], # ['constant', 'adaptive']
-            'hidden_layer_sizes': np.arange(5, X_train.shape[1] // 3, int(X_train.shape[1] * 0.1)), # np.arange(5, 50, 10)
-            'random_state': np.arange(0, 5, 1)} # np.arange(0, 10, 2)
+            'hidden_layer_sizes': [features // 4], #[features // x for x in range(2,10,2)]
+            'random_state': np.arange(0, 5, 1)
+        }
         clf = GridSearchCV(MLPClassifier(random_state=42), param_grid, n_jobs=-1, cv=5,
                           iid=True, verbose=verbose)
         clf.fit(X_train, y_train)
-        if verbose: print_cv_results(
-            clf, X_train, X_test, y_train, y_test,
+        if verbose: mu.print_cv_results(
+            clf, (X_train, X_test, y_train, y_test),
             feat_imp=False, top=20)
         best_params = clf.best_params_
 
@@ -204,10 +210,16 @@ def train_ds(context):
     print('MLPClassifier scores Train {}, Test {}'.format(
     clf2.score(X_train, y_train), clf2.score(X_test, y_test)))
 
+    best_params = { # best from GridSearch
+        'n_estimators': 50,
+        'max_features': features // 2,
+        'max_depth': 20,
+        'min_samples_split': 2,
+        'min_samples_leaf': 1,
+        'random_state': 2,
+        'n_jobs': -1}
     # ExtraTreesClassifier
-    clf3 = ExtraTreesClassifier(
-        n_estimators=100, max_depth=None,
-        min_samples_split=2, random_state=42)
+    clf3 = ExtraTreesClassifier(**best_params)
     clf3.fit(X_train, y_train)
     print('ExtraTreesClassifier scores Train {}, Test {}'.format(
     clf3.score(X_train, y_train), clf3.score(X_test, y_test)))
@@ -231,8 +243,6 @@ def predict_ds(context):
     verbose = context['verbose']
     (path, train_cols) = context['trained_cols']
 
-    px_close = get_mults_pricing(include, freq, verbose=verbose);
-    px_close.drop_duplicates(inplace=True)
     ds_idx, df_large = create_ds(px_close, context)
     pred_X, _, _, _, _ = pre_process_ds(df_large, context)
     print('pred_X.shape', pred_X.shape)
@@ -262,46 +272,12 @@ def predict_ds(context):
         bench_df.dropna(subset=[bench], inplace=True)
 
     # store in S3
-    s3_df = bench_df.reset_index(drop=False)
+    (path, fname) = context['s3_path']
+    s3_df = pred_df.reset_index(drop=False)
     rename_col(s3_df, 'index', 'pred_date')
-    csv_store(s3_df, 'recommend/', 'macro_risk_ML.csv')
+    csv_store(s3_df, path, csv_ext.format(fname))
 
     return bench_df
-
-def print_cv_results(clf, X_train, X_test, y_train, y_test, full_grid=False, feat_imp=True, top=20):
-    print(clf)
-    cvres = clf.cv_results_
-    print('BEST PARAMS:', clf.best_params_)
-    print('SCORES:')
-    print('clf.best_score_', clf.best_score_)
-    print('train {}, test {}'.format(
-        clf.score(X_train, y_train),
-        clf.score(X_test, y_test)))
-    if full_grid:
-        print('GRID RESULTS:')
-        for mean_score, params in zip(cvres["mean_test_score"], cvres["params"]):
-            print(round(mean_score, 3), params)
-    if feat_imp:
-        feature_importances = clf.best_estimator_.feature_importances_
-        print('SORTED FEATURES:')
-        print(sorted(zip(feature_importances, list(X_train.columns)), reverse=True)[:top])
-
-def visualize_predictions(pred_df):
-    pre_class_cols = filter_cols(pred_df.columns, "pred_class")
-    pred_df.loc[:,[bench] + pre_class_cols].plot(
-        secondary_y=pre_class_cols, figsize=(15, 5));
-    pred_df[fwd_ret_labels].plot.area(
-            title='Prediction Probabilities',
-            figsize=(15, 2), ylim=(0, 1), cmap='RdYlGn');
-    f'Confidence Mean: {pred_df["soft_confidence"].mean().round(3)}, Median {pred_df["soft_confidence"].median().round(3)}'
-
-def append_pricing(symbol, freq='1d', cols=None):
-    """ appends most recent pricing to data on S3"""
-    return appended_df
-
-def pull_latest_px(tickers):
-    """ get appended pricing from dataset """
-    return px_close
 
 #context/config for training and prediction
 context = {
@@ -313,18 +289,21 @@ context = {
     'test_size': .20,
     'predict_batch': 252,
     'ml_path': './ML/',
-    'grid_search': True,
+    'grid_search': False,
+    's3_path': ('recommend/', 'macro_risk_ML'),
     'verbose': 1}
+
+px_close = get_mults_pricing(include, freq, verbose=context['verbose']);
+px_close.drop_duplicates(inplace=True)
+print('px_close.shape', px_close.shape)
 
 if __name__ == '__main__':
     hook = sys.argv[1]
     if hook == 'train':
-        print('TRAINING MACRO RISK EXPOSURE:')
-        context['train_model'] = True
+        print('Training model using:', context)
         train_ds(context)
     elif hook == 'predict':
-        print('PREDICTING MACRO RISK EXPOSURE:')
-        context['train_model'] = False
+        print('Predicting model using:', context)
         pred_df = predict_ds(context)
         print(pred_df.tail(5).round(3).T)
     else: print('Invalid option, please try: train or predict')
