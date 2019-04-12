7c106b3b8af074e51853b562c84b85b287adba59
Victor Veiga <veigaco@gmail.com>
2019-04-12 08:41:27 -0700

    Added a few lines to check the recommend folder

--- bash/check_s3.sh
+++ bash/check_s3.sh
@@ -15,8 +15,21 @@ eval aws s3 ls fund-fin-data/pricing/1d/ | grep $(date +"%Y-%m-%d") | wc #count 
 echo "Count minute pricing objects for $(date +"%Y-%m-%d")"
 eval aws s3 ls fund-fin-data/pricing/1m/ | grep $(date +"%Y-%m-%d") | wc #count pricing objects for a date > 1304 companies
 
-echo "Count valuation water objects"
+echo "Count valuation waterfall objects"
 aws s3 ls fund-fin-data/valuation/waterfall/ | tail -n 3 #last 3 dates for quotes
 
 echo "List ML recommend objects"
-aws s3 ls fund-fin-data/recommend/ #last 3 dates for quotes
+echo "day_quote"
+aws s3 ls fund-fin-data/recommend/fdmn_ML-day_quote/ | tail -n 3
+echo "eps_estimates"
+aws s3 ls fund-fin-data/recommend/fdmn_ML-eps_estimates/ | tail -n 3
+echo "eps_trend"
+aws s3 ls fund-fin-data/recommend/fdmn_ML-eps_trend/ | tail -n 3 
+echo "fin_data"
+aws s3 ls fund-fin-data/recommend/fdmn_ML-fin_data/ | tail -n 3
+echo "key_statistics"
+aws s3 ls fund-fin-data/recommend/fdmn_ML-key_statistics/ | tail -n 3
+echo "macro_ML"
+aws s3 ls fund-fin-data/recommend/macro_ML/ | tail -n 3
+echo "micro_ML"
+aws s3 ls fund-fin-data/recommend/micro_ML/ | tail -n 3
