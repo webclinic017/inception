8fb49a419a77004ccdbbc54f9fc8bc66bf1b6cad
Victor Veiga <veigaco@gmail.com>
2019-05-12 22:32:26 -0700

    Clean up.

--- bash/check_s3.sh
+++ bash/check_s3.sh
@@ -12,8 +12,8 @@ aws s3 ls fund-fin-data/summary-categories/ #list last date of overrides
 echo "Count daily pricing objects for $(date +"%Y-%m-%d")"
 eval aws s3 ls fund-fin-data/pricing/1d/ | grep $(date +"%Y-%m-%d") | wc #count pricing objects for a date > 1304 companies
 
-echo "Count minute pricing objects for $(date +"%Y-%m-%d")"
-eval aws s3 ls fund-fin-data/pricing/1m/ | grep $(date +"%Y-%m-%d") | wc #count pricing objects for a date > 1304 companies
+# echo "Count minute pricing objects for $(date +"%Y-%m-%d")"
+# eval aws s3 ls fund-fin-data/pricing/1m/ | grep $(date +"%Y-%m-%d") | wc #count pricing objects for a date > 1304 companies
 
 echo "Count valuation waterfall objects"
 aws s3 ls fund-fin-data/valuation/waterfall/ | tail -n 3 #last 3 dates for quotes
@@ -25,13 +25,14 @@ echo "micro_ML"
 aws s3 ls fund-fin-data/recommend/micro_ML/ | tail -n 3
 echo "bottomup"
 aws s3 ls fund-fin-data/recommend/bottomup_ML/ | tail -n 3
-echo "day_quote"
-aws s3 ls fund-fin-data/recommend/fdmn_ML-day_quote/ | tail -n 3
-echo "eps_estimates"
-aws s3 ls fund-fin-data/recommend/fdmn_ML-eps_estimates/ | tail -n 3
-echo "eps_trend"
-aws s3 ls fund-fin-data/recommend/fdmn_ML-eps_trend/ | tail -n 3
-echo "fin_data"
-aws s3 ls fund-fin-data/recommend/fdmn_ML-fin_data/ | tail -n 3
-echo "key_statistics"
-aws s3 ls fund-fin-data/recommend/fdmn_ML-key_statistics/ | tail -n 3
+
+# echo "day_quote"
+# aws s3 ls fund-fin-data/recommend/fdmn_ML-day_quote/ | tail -n 3
+# echo "eps_estimates"
+# aws s3 ls fund-fin-data/recommend/fdmn_ML-eps_estimates/ | tail -n 3
+# echo "eps_trend"
+# aws s3 ls fund-fin-data/recommend/fdmn_ML-eps_trend/ | tail -n 3
+# echo "fin_data"
+# aws s3 ls fund-fin-data/recommend/fdmn_ML-fin_data/ | tail -n 3
+# echo "key_statistics"
+# aws s3 ls fund-fin-data/recommend/fdmn_ML-key_statistics/ | tail -n 3
