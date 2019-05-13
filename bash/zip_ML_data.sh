73b72d14aadd162946d0d2ee2ab48da4c5d814bb
Victor Veiga <veigaco@gmail.com>
2019-05-12 19:45:25 -0700

    Clean up.

--- bash/zip_ML_data.sh
+++ bash/zip_ML_data.sh
@@ -4,18 +4,18 @@ inference_IP=34.221.106.35
 models_fname=all_models.tar.gz
 tmp_fname=all_tmp.tar.gz
 
-echo '>> Tar models'
+echo '>> Zip models'
 cd $p/ML
 rm -f $model_fname
-tar -cvzf $model_fname *NN*
+tar -cvzf $model_fname *TF*
 
-echo '>> Tar temp files'
+echo '>> Unzip temp files'
 cd $p/tmp
 rm -f $tmp_fname
 tar -cvzf $tmp_fname .
 
 echo '>> Moving files to inference instance'
-scp -i ~ubuntu/.ssh/qc_infra.pem $p/ML/$models_fname ubuntu@$inference_IP:$p/ML/$models_fname
-scp -i ~ubuntu/.ssh/qc_infra.pem $p/tmp/$tmp_fname ubuntu@$inference_IP:$p/tmp/$tmp_fname
+scp -i ~/.ssh/qc_infra.pem $p/ML/$models_fname ubuntu@$inference_IP:$p/ML/$models_fname
+scp -i ~/.ssh/qc_infra.pem $p/tmp/$tmp_fname ubuntu@$inference_IP:$p/tmp/$tmp_fname
 
 cd $p
