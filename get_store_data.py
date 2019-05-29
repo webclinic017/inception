0577d1b6ed786c7db847e701289a803fabb8c6ef
Victor Veiga <veigaco@gmail.com>
2019-05-28 21:17:44 -0400

    Bug fixing, missing BaseDS import.

--- get_store_data.py
+++ get_store_data.py
@@ -2,6 +2,7 @@ import sys
 from utils.basic_utils import *
 from utils.unpack_summary import *
 from utils.pricing import load_px_close, get_pricing
+from utils.BaseDS import BaseDS
 
 if __name__ == '__main__':
     hook = sys.argv[1]
@@ -26,15 +27,22 @@ if __name__ == '__main__':
             try:
                 print('Retrieving {0} for {1}'.format(hook, t))
                 # get_pricing(t, '1m', '5d')
-                get_pricing(t, '1d', '15y')
+                # get_pricing(t, '1d', '15y')
             except Exception as e:
                 print(e)
         # persist closing price for current day
         temp_path, px_close_fname, px_vol_fname = 'tmp/', 'universe-px-ds', 'universe-px-vol-ds.h5'
-        print('Persisting pricing to {}'.format(temp_path + px_close_fname))
-        load_px_close(temp_path, px_close_fname, False)
+        # print('Persisting pricing to {}'.format(temp_path + px_close_fname))
+        # load_px_close(temp_path, px_close_fname, False)
         print(f'Persisting universe price and volume to {temp_path + px_vol_fname}')
-        BaseDS(path=temp_path, fname=px_vol_fname, load_ds=False, )
+        # baseDs = BaseDS(path=temp_path, fname=px_vol_fname, load_ds=False, )
+        baseDs = BaseDS(path=temp_path, fname=px_vol_fname, load_ds=True, )
+        px_close = baseDs.px_vol_df['close']
+        num_cols = numeric_cols(px_close)
+        px_close.loc[:, num_cols] = px_close[num_cols].astype(np.float32)
+        os.makedirs(temp_path, exist_ok=True)
+        px_close.to_parquet(path + fname)
+
     elif hook == 'options':
         sample_set = sys.argv[2]
         for t in config[sample_set]:
